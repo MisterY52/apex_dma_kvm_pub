061d905ce980ac8f4c67db173b2866c9373a86c6
Andrea Ianniciello <230103@studenti.unimore.it>
2020-10-06 16:52:59 +0200

    Updated offsets

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 UnknownCheats thread: https://www.unknowncheats.me/forum/apex-legends/406426-kvm-vmread-apex-esp-aimbot.html
 
-Game version: v3.0.3.454
+Game version: v3.0.4.150
--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -1,5 +1,5 @@
-#define OFFSET_ENTITYLIST			0x18b9a98
-#define OFFSET_LOCAL_ENT			0x1c684b8 //LocalPlayer
+#define OFFSET_ENTITYLIST			0x18ad3a8
+#define OFFSET_LOCAL_ENT			0x1c5bcc8 //LocalPlayer
 
 #define OFFSET_TEAM					0x430
 #define OFFSET_HEALTH				0x420
@@ -10,7 +10,7 @@
 #define OFFSET_VISIBLE_TIME         0x1A6C
 
 #define OFFSET_LIFE_STATE			0x770  //>0 = dead
-#define OFFSET_BLEED_OUT_STATE		0x2670 //>0 = knocked
+#define OFFSET_BLEED_OUT_STATE		0x2610 //>0 = knocked
 
 #define OFFSET_ORIGIN				0x14c //m_vecAbsOrigin
 #define OFFSET_BONES				0xF18 //m_bConstrainBetweenEndpoints
@@ -18,16 +18,19 @@
 #define OFFSET_CAMERAPOS			0x1E6C
 #define OFFSET_VIEWANGLES			0x24A0
 #define OFFSET_BREATH_ANGLES		OFFSET_VIEWANGLES - 0x10
-#define OFFSET_OBSERVER_MODE		0x3304
-#define OFFSET_OBSERVING_TARGET		0x3308
+#define OFFSET_OBSERVER_MODE		0x32bc
+#define OFFSET_OBSERVING_TARGET		0x32c0
 
 #define OFFSET_MATRIX				0x1b3bd0
-#define OFFSET_RENDER				0x40deec8 
+#define OFFSET_RENDER				0x40d5d98 
 
 #define OFFSET_WEAPON				0x1a0c //m_latestPrimaryWeapons
-#define OFFSET_BULLET_SPEED         0x1e08
-#define OFFSET_BULLET_SCALE         0x1e10
+#define OFFSET_BULLET_SPEED         0x1e0c
+#define OFFSET_BULLET_SCALE         0x1e14
 
-#define OFFSET_ITEM_GLOW            0x290
-#define OFFSET_GLOW_ENABLE          0x330 //7 = enabled
-#define OFFSET_GLOW_THROUGH_WALLS   0x340 //2 = enabled
+#define OFFSET_ITEM_GLOW            0x2A8
+
+#define OFFSET_GLOW_T1              0x262 //16256 = enabled, 0 = disabled 
+#define OFFSET_GLOW_T2              0x2c4 //1193322764 = enabled, 0 = disabled 
+#define OFFSET_GLOW_ENABLE          0x350 //7 = enabled, 2 = disabled
+#define OFFSET_GLOW_THROUGH_WALLS   0x360 //2 = enabled, 5 = disabled
