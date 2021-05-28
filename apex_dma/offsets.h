075ee9fe8c85b362f58b604b721c54f52b7667ac
MisterY52 <11368116+MisterY52@users.noreply.github.com>
2021-05-28 17:54:10 +0200

    Updated offsets

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -7,7 +7,7 @@
 
     #define OFFSET_ENTITYLIST			0x18eda78
     #define OFFSET_LOCAL_ENT			0x1c9d198 //LocalPlayer
-    #define OFFSET_NAME_LIST            0x81af540
+    #define OFFSET_NAME_LIST            0x81af640
     #define OFFSET_THIRDPERSON          0x018d42d0 + 0x6c //thirdperson_override + 0x6c
 
     #define OFFSET_TEAM					0x448 //m_iTeamNum
@@ -32,7 +32,7 @@
     #define OFFSET_OBSERVING_TARGET		0x3350 //m_hObserverTarget
 
     #define OFFSET_MATRIX				0x1b3bd0
-    #define OFFSET_RENDER				0x40bf368
+    #define OFFSET_RENDER				0x40bf468
 
     #define OFFSET_WEAPON				0x1a6c //m_latestPrimaryWeapons
     #define OFFSET_BULLET_SPEED         0x1ED0
@@ -50,7 +50,8 @@
 
     #define OFFSET_ENTITYLIST			0x18eda78
     #define OFFSET_LOCAL_ENT			0x1c9d198 //LocalPlayer
-    #define OFFSET_NAME_LIST            0x81af540
+    #define OFFSET_NAME_LIST            0x81af640
+    #define OFFSET_THIRDPERSON          0x018d42d0 + 0x6c //thirdperson_override + 0x6c
 
     #define OFFSET_TEAM					0x448 //m_iTeamNum
     #define OFFSET_HEALTH				0x438 //m_iHealth
@@ -74,7 +75,7 @@
     #define OFFSET_OBSERVING_TARGET		0x3350 //m_hObserverTarget
 
     #define OFFSET_MATRIX				0x1b3bd0
-    #define OFFSET_RENDER				0x40bf368
+    #define OFFSET_RENDER				0x40bf468
 
     #define OFFSET_WEAPON				0x1a6c //m_latestPrimaryWeapons
     #define OFFSET_BULLET_SPEED         0x1ED0
