898f1f25aa426bcf81b1e89b466fab264af6e858
MisterY52 <11368116+MisterY52@users.noreply.github.com>
2021-09-23 10:43:05 +0200

    Updated offsets

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -5,10 +5,10 @@
 
 #if VERSION == STEAM
 
-    #define OFFSET_ENTITYLIST			0x192f108
-    #define OFFSET_LOCAL_ENT			0x1cdea28 //LocalPlayer
-    #define OFFSET_NAME_LIST            0xb73ec60
-    #define OFFSET_THIRDPERSON          0x019154d0 + 0x6c //thirdperson_override + 0x6c
+    #define OFFSET_ENTITYLIST			0x1930108
+    #define OFFSET_LOCAL_ENT			0x1cdfa28 //LocalPlayer
+    #define OFFSET_NAME_LIST            0xb73fc60
+    #define OFFSET_THIRDPERSON          0x019164d0 + 0x6c //thirdperson_override + 0x6c
 
     #define OFFSET_TEAM					0x448 //m_iTeamNum
     #define OFFSET_HEALTH				0x438 //m_iHealth
@@ -32,7 +32,7 @@
     #define OFFSET_OBSERVING_TARGET		0x33b8 //m_hObserverTarget
 
     #define OFFSET_MATRIX				0x1b3bd0
-    #define OFFSET_RENDER				0x75017e0
+    #define OFFSET_RENDER				0x75027e0
 
     #define OFFSET_WEAPON				0x1a6c //m_latestPrimaryWeapons
     #define OFFSET_BULLET_SPEED         0x1ee0 //CWeaponX!m_flProjectileSpeed
@@ -48,10 +48,10 @@
 
 #elif VERSION == ORIGIN
 
-    #define OFFSET_ENTITYLIST			0x192f108
-    #define OFFSET_LOCAL_ENT			0x1cdea28 //LocalPlayer
-    #define OFFSET_NAME_LIST            0xb73ec60
-    #define OFFSET_THIRDPERSON          0x019154d0 + 0x6c //thirdperson_override + 0x6c
+    #define OFFSET_ENTITYLIST			0x1930108
+    #define OFFSET_LOCAL_ENT			0x1cdfa28 //LocalPlayer
+    #define OFFSET_NAME_LIST            0xb73fc60
+    #define OFFSET_THIRDPERSON          0x019164d0 + 0x6c //thirdperson_override + 0x6c
 
     #define OFFSET_TEAM					0x448 //m_iTeamNum
     #define OFFSET_HEALTH				0x438 //m_iHealth
@@ -75,7 +75,7 @@
     #define OFFSET_OBSERVING_TARGET		0x33b8 //m_hObserverTarget
 
     #define OFFSET_MATRIX				0x1b3bd0
-    #define OFFSET_RENDER				0x75017e0
+    #define OFFSET_RENDER				0x75027e0
 
     #define OFFSET_WEAPON				0x1a6c //m_latestPrimaryWeapons
     #define OFFSET_BULLET_SPEED         0x1ee0 //CWeaponX!m_flProjectileSpeed
